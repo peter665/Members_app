45d90ad55357c75b4c122fc5f8063edd1ee5040c
peter <peto.mester@gmail.com>
2017-04-26 19:39:39 +0200

    Added link disable/enable feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,23 @@ class PostsController < ApplicationController
   before_filter :logged_in?, exept: [:index]
 
   def new
-    
+    unless current_user
+      flash[:danger] = 'You need to login/signup to make posts!'
+      redirect_to root_path
+    end
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
-    if @post.save
-      flash[:success] = 'Post has been created.'
-      redirect_to posts_index_path
+    if current_user
+      @post = Post.new(post_params)
+      @post.user_id = current_user.id
+      if @post.save
+        flash[:success] = 'Post has been created.'
+        redirect_to posts_index_path
+      else
+        render :new
+      end
     else
-
       render :new
     end
   end
